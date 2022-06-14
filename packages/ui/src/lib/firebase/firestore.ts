371c388765736beebf061ae6fa1e5efe5895b833
Tommy Odom <tommy.odom@gmail.com>
2022-06-13 21:19:36 -0400

    fix: use yn to handle falsy string env vars

--- packages/ui/src/lib/firebase/firestore.ts
+++ packages/ui/src/lib/firebase/firestore.ts
@@ -1,8 +1,9 @@
 import { firebaseApp } from "./app";
 import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
+import yn from "yn";
 
 export const firestore = getFirestore(firebaseApp);
 
-if (!!import.meta.env.VITE_FIREBASE_EMULATOR) {
+if (yn(import.meta.env.VITE_FIREBASE_EMULATOR)) {
   connectFirestoreEmulator(firestore, "localhost", 8080);
 }
--- packages/ui/src/lib/firebase/functions.ts
+++ packages/ui/src/lib/firebase/functions.ts
@@ -1,10 +1,11 @@
 import { getFunctions, connectFunctionsEmulator } from "firebase/functions";
 import { firebaseApp } from "./app";
+import yn from "yn";
 
 export * from "firebase/functions";
 
 export const functions = getFunctions(firebaseApp);
 
-if (!!import.meta.env.VITE_FIREBASE_EMULATOR) {
+if (yn(import.meta.env.VITE_FIREBASE_EMULATOR)) {
   connectFunctionsEmulator(functions, "localhost", 5001);
 }
